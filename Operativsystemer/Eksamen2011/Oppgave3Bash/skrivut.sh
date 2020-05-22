50e03a3e20996f212fbe3bf93fb7aa161b2364f2
andreas <andreas@DESKTOP-IRVMQ5I.localdomain>
2020-05-22 15:45:52 +0200

    latest

---
+++ Operativsystemer/Eksamen2011/Oppgave3Bash/skrivut.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+for i in {1..4} 
+do 
+	echo Linje $((i*2))
+done
---
+++ Operativsystemer/Eksamen2012/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+	int mat[5000][5000];
+	for(int i = 0;i < 5000;i++){
+	      for(int j = 0;j < 5000;j++){
+		     mat[i][j] = 5;
+				          }
+	}
+	}
